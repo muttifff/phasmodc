164f5f56b43e91cf2d3af74f142a951d81e88a81
muttifff <59918469+muttifff@users.noreply.github.com>
2021-08-13 13:46:16 +0300

    Update discourse_events_handlers.rb

--- lib/discourse_events_handlers.rb
+++ lib/discourse_events_handlers.rb
@@ -7,11 +7,11 @@ module ::DiscordBot::DiscourseEventsHandlers
         posted_category = post.topic.category.id
         posted_category_name = Category.find_by(id: posted_category).name
         if post_listening_categories.include?(posted_category.to_s) then
-          message = "There's a new Post in the '#{posted_category_name}' Category on Discourse: #{Discourse.base_url + post.url}"
+          message = "Forum adresimizde '#{posted_category_name}' Kategorisinde yeni bir konu: #{Discourse.base_url + post.url}"
           ::DiscordBot::Bot.discord_bot.send_message(SiteSetting.discord_bot_announcement_channel_id, message)
         else
           if topic_listening_categories.include?(posted_category.to_s) && post.post_number = 1 then
-            message = "There's a new Topic in the '#{posted_category_name}' Category on Discourse: #{Discourse.base_url + post.url}"
+            message = "Forum adresimizde '#{posted_category_name}' Kategorisinde yeni bir konu: #{Discourse.base_url + post.url}"
             ::DiscordBot::Bot.discord_bot.send_message(SiteSetting.discord_bot_announcement_channel_id, message)
           end
         end
